d3c27698d07955f67c06439ac7ae6a7c9042b4a0
Jeff McDonald <jwmcdonald85@gmail.com>
2023-11-24 21:56:03 -0500

    Combine objects, arrays, and functions more than one level deep

--- combining_datatypes.js
+++ combining_datatypes.js
@@ -4,6 +4,9 @@
 
   // -------------------- Section 2:  Combining Datatypes -------------------- //
 
+
+
+
 // ----- Combine objects, arrays, and functions ----- //
 
 // 1. -------------------------------------- //
@@ -67,3 +70,54 @@ const apartmentBuilding = [
 
 // Logging a tenant
 console.log(apartmentBuilding[1][1])
+
+
+
+
+// ----- Combine objects, arrays, and functions more than one level deep ----- //
+
+// 1.
+// Creating a function 
+const knit = () => {
+    return {
+        item: "sweater",
+        size: "large",
+        team: "Sabres"
+    }
+}
+
+// Calling the property "team" from the function knit to return Sabres
+console.log(knit().team)
+
+
+// 2.
+// Create a function
+const crayonSelector = () => {
+    return {
+        // Create an object that calls an array
+        crayons: crayonBox.sticks
+    }
+}
+
+// Log the color "Orange"
+console.log(crayonSelector().crayons[1])
+
+
+// 3.
+// Create a function
+function powerButton() {
+    // Creating another function inside the first on that logs a statement
+    function options() {
+        // Logging a statement
+        console.log("Select a song");
+    }
+    // Returning the sub function
+    return options;
+}
+
+// Envoking the primary function and storing the results in a variable
+const powerButtontoggle = powerButton();
+// Envoking the variable
+powerButtontoggle()
+
+
